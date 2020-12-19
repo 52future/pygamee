07726b61c048a7da3d3a7d40fa587f44098225ec
52future <thswkdud2002@naver.com>
2020-12-20 00:36:02 +0900

    python game_

--- 6_gameover.py
+++ 6_gameover.py
@@ -1,12 +1,12 @@
 import os
 import pygame
-##############################################################
-# 기본 초기화 (반드시 해야 하는 것들)
+##################
+# 기본 초기화
 pygame.init()
 
 # 화면 크기 설정
-screen_width = 640 # 가로 크기
-screen_height = 480 # 세로 크기
+screen_width = 640 # 가로
+screen_height = 480 # 세로
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # 화면 타이틀 설정
@@ -14,9 +14,9 @@ pygame.display.set_caption("Pang")
 
 # FPS
 clock = pygame.time.Clock()
-##############################################################
+##################
 
-# 1. 사용자 게임 초기화 (배경 화면, 게임 이미지, 좌표, 속도, 폰트 등)
+# 1. 사용자 게임 초기화
 current_path = os.path.dirname(__file__) # 현재 파일의 위치 반환
 image_path = os.path.join(current_path, "images") # images 폴더 위치 반환
 
@@ -36,7 +36,7 @@ character_height = character_size[1]
 character_x_pos = (screen_width / 2) - (character_width / 2)
 character_y_pos = screen_height - character_height - stage_height
 
-# 캐릭터 이동 방향
+
 character_to_x = 0
 
 # 캐릭터 이동 속도
@@ -47,23 +47,23 @@ weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
 weapon_size = weapon.get_rect().size
 weapon_width = weapon_size[0]
 
-# 무기는 한 번에 여러 발 발사 가능
+# 무기 여러발
 weapons = []
 
 # 무기 이동 속도
 weapon_speed = 10
 
-# 공 만들기 (4개 크기에 대해 따로 처리)
+# 공 만들기 (4개)
 ball_images = [
     pygame.image.load(os.path.join(image_path, "balloon1.png")),
     pygame.image.load(os.path.join(image_path, "balloon2.png")),
     pygame.image.load(os.path.join(image_path, "balloon3.png")),
     pygame.image.load(os.path.join(image_path, "balloon4.png"))]
 
-# 공 크기에 따른 최초 스피드
+# 공크기에따라 스피드다르게
 ball_speed_y = [-18, -15, -12, -9] # index 0, 1, 2, 3 에 해당하는 값
 
-# 공들
+# 공
 balls = []
 
 # 최초 발생하는 큰 공 추가
@@ -122,14 +122,14 @@ while running:
         character_x_pos = screen_width - character_width
 
     # 무기 위치 조정
-    # 100, 200 -> 180, 160, 140, ...
-    # 500, 200 -> 180, 160, 140, ...
+    # 100, 200 -> 180, 160, 140,...
+    # 500, 200 -> 180, 160, 140, ..
     weapons = [ [w[0], w[1] - weapon_speed] for w in weapons] # 무기 위치를 위로
 
-    # 천장에 닿은 무기 없애기
+    # 천장에 닿은 무기 x
     weapons = [ [w[0], w[1]] for w in weapons if w[1] > 0]
     
-    # 공 위치 정의
+    # 공 위치
     for ball_idx, ball_val in enumerate(balls):
         ball_pos_x = ball_val["pos_x"]
         ball_pos_y = ball_val["pos_y"]
@@ -139,12 +139,12 @@ while running:
         ball_width = ball_size[0]
         ball_height = ball_size[1]
 
-        # 가로벽에 닿았을 때 공 이동 위치 변경 (튕겨 나오는 효과)
+        # 가로에 닿았을 때 공 이동위치 변경 (튕겨 나오는 효과)
         if ball_pos_x < 0 or ball_pos_x > screen_width - ball_width:
             ball_val["to_x"] = ball_val["to_x"] * -1
 
         # 세로 위치
-        # 스테이지에 튕겨서 올라가는 처리
+        # 스테이지에 튕겨서 올라가는처리
         if ball_pos_y >= screen_height - stage_height - ball_height:
             ball_val["to_y"] = ball_val["init_spd_y"]
         else: # 그 외의 모든 경우에는 속도를 증가
@@ -153,7 +153,7 @@ while running:
         ball_val["pos_x"] += ball_val["to_x"]
         ball_val["pos_y"] += ball_val["to_y"]
 
-    # 4. 충돌 처리
+    # 4. 충돌처리
 
     # 캐릭터 rect 정보 업데이트
     character_rect = character.get_rect()
